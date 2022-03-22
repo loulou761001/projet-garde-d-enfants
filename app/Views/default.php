1b9e04a11d590961b31be6b91c4bbed112ba3006
Louis Cherik <louis.cherik@gmail.com>
2022-03-22 10:01:53 +0100

    ajout du CSS et du layout

---
+++ app/Views/default.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Ticrocos</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="<?= base_url('/assets/css/style.css'); ?>" />
+</head>
+<body>
+
+<header>
+    <ul class="flex wrap sb">
+        <li>
+            <a href="home"><img src="<?= base_url('assets/imgs/ticrocos_logo.svg'); ?>" alt="Logo Ticrocos"></a>
+        </li>
+        <li>
+            <div class="flex">
+                <a href="">Inscription</a>
+                <a href="">Connexion</a>
+            </div>
+        </li>
+    </ul>
+
+</header>
+<?= $this->renderSection('content') ?>
+</body>
+</html>
--- app/Views/home.php
+++ app/Views/home.php
@@ -1,4 +1,13 @@
+<?= $this->extend('default') ?>
+
+<?= $this->section('content'); ?>
+
+
 <h1>HOME</h1>
+
+
+
 
 <?php
 var_dump($parents);
+$this->endSection() ;
